b34ef6648454478473ad4faef45247cc5664597d
Robert Laganiere <robert@laganiere.name>
2013-11-06 10:37:49 -0500

    erase-remove idiom updated

--- classesParam/iterateurs3/main.cpp
+++ classesParam/iterateurs3/main.cpp
@@ -2,22 +2,66 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <algorithm>
 
 using namespace std;
 
+template <typename T>
+void affiche(const T &v) {
+
+    T::const_iterator it= v.begin();
+
+    while (it!=v.end()) {
+
+        cout << *it;
+        ++it;
+
+        if (it!=v.end())
+            cout << ", ";
+    }
+
+    cout << endl;
+}
+
 int main()
 {
-    // le cas erase-remove
     vector<int> v2(5, 1);
     v2[1] = v2[3] = 99;
-    vector<int>::iterator p;
-    p = remove(v2.begin(), v2.end(), 99);
-    // déplace les éléments *q du conteneur de façon
-    // à ce que [v.begin(), p[, on ne trouve que des
-    // éléments tels que !(*q == valeur)
 
+    affiche(v2);
+
+    vector<int>::iterator p;
+    p = std::remove(v2.begin(), v2.end(), 99);
+    // deplace les elements *q du conteneur de facon
+    // a  ce que [v.begin(), p[, on ne trouve que des
+    // elements tels que !(*q == valeur)
+
+    affiche(v2);
+
+    vector<int> v3(v2.begin(),p);
+    affiche(v3);
+
+    // attention a ne pas deborder du conteneur
+    v2.erase(v2.begin()+1,v2.end()-2);
+    affiche(v2);
+
+    v2.push_back(4);
+    v2.push_back(99);
+    v2.push_back(5);
+    v2.push_back(6);
+
+    affiche(v2);
+    // l'idiome erase-remove
     v2.erase(remove(v2.begin(), v2.end(), 99), v2.end());
+    affiche(v2);
 
     list<int> li(5, 1);
+    li.push_back(99);
+    li.push_front(99);
+    li.push_front(4);
+
+    affiche(li);
+    // list a son propre remove
     li.remove(99);
+    affiche(li);
 }
